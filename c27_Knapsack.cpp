00d3f1c843b658fa08e390b127f0405a6d1e5d96
ysgclight <37142664+ysgclight@users.noreply.github.com>
2019-04-06 00:30:11 -0400

    Update c27_Knapsack.cpp

--- c27_Knapsack.cpp
+++ c27_Knapsack.cpp
@@ -4,6 +4,7 @@
 
 using std::cout;
 using std::endl;
+using std::cin;
 using std::vector;
 
 #define print(a) cout<<a<<endl;
@@ -26,7 +27,11 @@ int knapsack(vector<std::pair<int,int>>::iterator begin,\
         return 0;
     }
     else if (max_val < (end-1)->second){ // dont forget this case, otherwise the answer is wrong!
-        return 0;
+        std::pair<int,int> p = std::make_pair(end-begin-1,max_val);
+        if (history.find(p) == history.end()){
+            history[p] = knapsack(begin,end-1,max_val,history);
+        }
+        return history[p];
     }
     else{
         std::pair<int,int> p1 = std::make_pair(end-begin-1,max_val);
@@ -46,8 +51,27 @@ int main(){
     // vector<std::pair<int,int>> val_wt = {{60,10}, {100,20}, {120,30}}; // optimal => 220
     // int max_val = 50;
 
-    vector<std::pair<int,int>> val_wt = {{3,4}, {2,3}, {4,2},{4,3}}; // optimal => 8
-    int max_val = 6;
+
+    // vector<std::pair<int,int>> val_wt = {{3,4}, {2,3}, {4,2},{4,3}}; // optimal => 8
+    // int max_val = 6;
+
+
+    // freopen("knapsack1.txt","r",stdin); // optimal => 2493893
+    freopen("knapsack_big.txt","r",stdin); // optimal => 4243395
+    int knapsack_size;
+    int number_of_items;
+    cin >> knapsack_size >> number_of_items;
+    print("knapsack_size: " << knapsack_size << ", number_of_items: " << number_of_items);
+    int value;
+    int weight;
+    vector<std::pair<int,int>> val_wt;
+    while(cin >> value >> weight){
+        val_wt.emplace_back(std::make_pair(value,weight));
+        // print(val_wt.back().first << " - " << val_wt.back().second);
+    }
+    // print(val_wt.size());
+    int max_val = knapsack_size;
+
 
 
     std::unordered_map<std::pair<int,int>,int,pair_hash> history;
